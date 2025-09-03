d6276bb0cd19c2e62a69ff8536660a62af1884b8
sourbhpal-del <sourbh.pal@gmail.com>
2025-09-03 23:37:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import '@/styles/globals.css'
 import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div>
       <Navbar />
-      <main className="flex items-center justify-center py-20">
-        <h1 className="text-3xl font-bold text-blue-600">
-          🚀 Kids App with Tailwind is Working!
-        </h1>
-      </main>
+      <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
+        🚀 Kids App with Tailwind is Working!
+      </h1>
     </div>
-  )
+  );
 }
-
