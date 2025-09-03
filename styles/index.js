70176818a1e4a2e3a21befa82779cf0ac31e53f7
sourbhpal-del <sourbh.pal@gmail.com>
2025-09-03 23:10:46 +0530

    Delete styles/index.js

--- styles/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        🚀 Kids App with Tailwind is Working!
-      </h1>
-    </div>
-  );
-}
