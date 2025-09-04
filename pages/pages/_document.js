13879358e311beb383e738421144c5a28a631285
sourbhpal-del <sourbh.pal@gmail.com>
2025-09-04 21:53:39 +0530

    Update _document.js

--- pages/pages/_document.js
+++ pages/pages/_document.js
@@ -1,31 +1,4 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* PWA Support */}
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#2563eb" />
-
-        {/* iOS fullscreen + safe-area */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <meta name="mobile-web-app-capable" content="yes" />
-
-        {/* iOS/Android app icons (make sure icons exist in /public) */}
-        <link rel="apple-touch-icon" href="/icon-192.png" />
-        <link rel="icon" sizes="192x192" href="/icon-192.png" />
-        <link rel="icon" sizes="512x512" href="/icon-512.png" />
-      </Head>
-      <body className="bg-gray-50 text-gray-900">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+<link rel="manifest" href="/manifest.json" />
+<link rel="icon" href="/icon-192.png" sizes="192x192" />
+<link rel="icon" href="/icon-512.png" sizes="512x512" />
+<meta name="theme-color" content="#2563eb" />
