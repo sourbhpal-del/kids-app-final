e66e71a09fd89b469f69fc34e305332fb81dd916
sourbhpal-del <sourbh.pal@gmail.com>
2025-09-04 09:38:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Navbar from '../components/Navbar';
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
-        🚀 Kids App with Tailwind is Working!
-      </h1>
+      <h1>🎉 Kids Learning App</h1>
     </div>
   );
 }
