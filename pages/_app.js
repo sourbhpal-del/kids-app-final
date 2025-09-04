be09cdde20ca1d336a6b121b9ae238429bdd2ae6
sourbhpal-del <sourbh.pal@gmail.com>
2025-09-04 22:52:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
-import "../styles/globals.css";
-import Navbar from "../components/Navbar";
+// pages/_app.js
+import "@/styles/globals.css"; // Global styles (Tailwind + custom)
+import Navbar from "@/components/Navbar"; // Navigation bar
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Navbar always on top */}
       <Navbar />
-      <div className="p-4">
+
+      {/* Page content */}
+      <main className="max-w-6xl mx-auto px-4 py-6">
         <Component {...pageProps} />
-      </div>
+      </main>
     </>
   );
 }
